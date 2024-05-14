ebb9b47a1fe03e7c7c1dc4afed9b5e490ada82af
tjanuki <t.januki@gmail.com>
2024-05-14 14:52:15 -0400

    Add article create routes and form

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  resources :articles
+
 
   # Defines the root path route ("/")
   # root "posts#index"
