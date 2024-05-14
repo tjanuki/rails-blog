b593b8e3906ce771fe8e040e39c01deb06a0c59e
tjanuki <t.januki@gmail.com>
2024-05-13 20:08:44 -0400

    Add article show route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
 
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
