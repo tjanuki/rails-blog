6390c1d8ec3aedfd580fee1c4f2efc364405f73f
tjanuki <t.januki@gmail.com>
2024-05-16 19:31:16 -0400

    Add Comment model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
 
-  resources :articles
-
+  resources :articles do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
