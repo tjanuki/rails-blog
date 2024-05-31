6c13d350273e82d9ed2dfdd245a1fc81e88d6463
tjanuki <t.januki@gmail.com>
2024-05-31 16:09:21 -0400

    Add articles_controller test cases

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
+      flash[:notice] = "Article was successfully created."
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -22,6 +22,15 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_equal 1, Article.where(title: "can create").count
   end
 
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { title: "Some title", body: "Some body with discription", status: "public"}}
+    end
+
+    assert_redirected_to article_path(Article.last)
+    assert_equal "Article was successfully created.", flash[:notice]
+  end
+
   test "should get show" do
     get article_url(Article.first), headers: { "HTTP_REFERER" => "http://example.com/home" }
 
@@ -40,5 +49,12 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "h1", "can update"
     assert_select "p", "article successfully."
+  end
+
+  test "should get index, check with controller method" do
+    get articles_url
+
+    assert_equal "index", @controller.action_name
+    assert_match "Articles", @response.body
   end
 end
